8df5efe4e0425aabd77fd54da721d9ac876117aa
CaroDute <caro.duteurtre@orange.fr>
2024-09-24 19:30:09 +0200

    ajout style

--- src/pages/Bibliotheque.jsx
+++ src/pages/Bibliotheque.jsx
@@ -4,40 +4,42 @@ import Header from "../components/Header";
 
 const Bibliotheque = () => {
   return (
-    <div className="library">
-      <Header />
-      <div className="search">
-        <img src="./src/assets/images/Loupe.png" alt="" />
-        <Recherche />
-      </div>
-      <div>
-        <div>
-          <h2>Ma Bibliothèque</h2>
+    <div>
+      <div className="library">
+        <Header />
+        <div className="search">
+          <img src="./src/assets/images/Loupe.png" alt="" />
+          <Recherche />
         </div>
         <div>
-          <h3>Filtres</h3>
-          <ul>
-            <li>
-              <button>favoris</button>
-            </li>
-            <li>
-              <button>Lu</button>
-            </li>
-            <li>
-              <button>En cours</button>
-            </li>
-            <li>
-              <button>WhishList</button>
-            </li>
-            <li>
-              <button>Filtres avancés</button>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h3>Mes livres</h3>
+          <div>
+            <h2>Ma Bibliothèque</h2>
+          </div>
+          <div>
+            <h3>Filtres</h3>
+            <ul>
+              <li>
+                <button>favoris</button>
+              </li>
+              <li>
+                <button>Lu</button>
+              </li>
+              <li>
+                <button>En cours</button>
+              </li>
+              <li>
+                <button>WhishList</button>
+              </li>
+              <li>
+                <button>Filtres avancés</button>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Mes livres</h3>
 
-          <Livres />
+            <Livres />
+          </div>
         </div>
       </div>
     </div>
